59639b25049c0aaae185e7bc12301343f60ad938
Taikom <moztaya@gmail.com>
2020-10-07 11:40:53 +0300

    задания глав 2.3, 2.4

---
+++ st_2_3_6.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time
+import math
+import os
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+
+    x = browser.find_element_by_id("input_value").text
+
+    input = browser.find_element_by_id("answer")
+    input.send_keys(calc(x))
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
---
+++ st_2_4_8.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    # говорим WebDriver ждать все элементы в течение 5 секунд
+    browser.implicitly_wait(5)
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    price = WebDriverWait(browser, 12).until(
+            EC.text_to_be_present_in_element((By.ID,"price"), "100"))
+    button=browser.find_element_by_id("book")
+    button.click()
+
+    x = browser.find_element_by_id("input_value").text
+
+    input = browser.find_element_by_id("answer")
+    input.send_keys(calc(x))
+
+    button_submit = browser.find_element_by_id("solve")
+    button_submit.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
